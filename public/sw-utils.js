676a317b75d4a7057333727b5061ce36f20c3f8f
Chen Peleg <chenpeleg@gmail.com>
2025-04-30 10:09:30 +0300

    Add service worker utility classes and implement caching logic
    - Introduced ServiceWorkerConstants class to manage service worker constants and cache names.
    - Added ServiceWorkerDebug class for logging and debugging purposes.
    - Implemented cache clearing logic in the service worker during activation.
    - Set up event handlers for install, activate, and fetch events in the service worker.
    - Enhanced logging to provide insights during service worker operations.

---
+++ public/sw-utils.js
@@ -0,0 +1,67 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class ServiceWorkerConstants {
+    static  APP_PREFIX = 'rtmfort_';
+    version = 'version_00';
+
+    constructor({version}) {
+        this.version = version;
+    }
+
+    static _swName = 'Rotem-fortune';
+
+    get swName() {
+        return ServiceWorkerConstants._swName;
+
+    }
+
+    get CACHE_CORE_NAME() {
+        return ServiceWorkerConstants.APP_PREFIX + 'core'; // core
+    }
+
+    get CACHE_NAME() {
+        return ServiceWorkerConstants.APP_PREFIX + this.version;
+    };
+
+}
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class ServiceWorkerDebug {
+    static isDebugMode = false;
+    static intervalId = null;
+    static logsRecord = [];
+    static debounceTime = 1000;
+
+    static isLoggingToConsole() {
+        return self.location.hostname === 'localhost' && !ServiceWorkerDebug.isDebugMode;
+    }
+
+    static log(...args) {
+        if (!ServiceWorkerDebug.isLoggingToConsole()) {
+            return;
+        }
+        console.log(...args);
+    }
+
+    static debounceLog(...args) {
+        if (args && args.length === 1) {
+            ServiceWorkerDebug.logsRecord.push(args[0]);
+        } else {
+            ServiceWorkerDebug.logsRecord.push(args);
+        }
+        if (!ServiceWorkerDebug.intervalId) {
+            ServiceWorkerDebug.intervalId = setTimeout(() => {
+                if (ServiceWorkerDebug.logsRecord.length > 10) {
+                    ServiceWorkerDebug.log({logRecords: ServiceWorkerDebug.logsRecord});
+                } else {
+
+                    ServiceWorkerDebug.log(ServiceWorkerDebug.logsRecord);
+                }
+                ServiceWorkerDebug.logsRecord = [];
+                clearTimeout(ServiceWorkerDebug.intervalId);
+            }, ServiceWorkerDebug.debounceTime);
+        }
+
+    }
+}
+
---
+++ public/sw.js
@@ -0,0 +1,92 @@
+// eslint-disable-next-line no-undef
+importScripts('./sw-utils.js');
+
+const DEBUG_MODE = true;
+
+const swConstants = // eslint-disable-next-line no-undef
+    new ServiceWorkerConstants({ version: 'version_09' });
+
+// eslint-disable-next-line no-undef
+class Debug extends ServiceWorkerDebug {}
+
+Debug.isDebugMode = !DEBUG_MODE;
+
+
+const clearCaches = async () => {
+    Debug.log('Checking and Clearing old caches...');
+    const keys = await caches.keys();
+    const allKeysToDelete = keys.filter(function(key) {
+        return key.indexOf(swConstants.CACHE_NAME) !== 0 && key !==
+            swConstants.CACHE_CORE_NAME;
+    });
+    let letCachesFound = 0;
+    await Promise.all(allKeysToDelete.map((key) => {
+        Debug.log('Removing old cache: ' + key);
+        letCachesFound++;
+        return caches.delete(key);
+    }));
+    if (letCachesFound === 0) {
+        Debug.log('No old caches found');
+    } else {
+        Debug.log(`${letCachesFound} Old caches cleared`);
+    }
+};
+
+/**
+ * @param { Event & {waitUntil : (Promise) =>Promise  }} activationEvent
+ * @return {Promise<void>}
+ */
+const activateEventHandler = async (activationEvent) => {
+    Debug.log(`[Service Worker] Activating Service Worker version ${swConstants.version} ....`);
+    await activationEvent.waitUntil(clearCaches());
+    return self.clients.claim();
+};
+/**
+ *
+ * @return {Promise<void>}
+ */
+const installEventHandler = async () => {
+    Debug.log(`[Service Worker] Installing Service Worker ${swConstants.swName}`);
+    return self.skipWaiting();
+};
+
+
+/**
+ * @param {FetchEvent} fetchEvent
+ * @return {Promise<FetchEvent>}
+ */
+const fetchEventHandler = async (fetchEvent) => {
+    const responsePromise = async (fetchEvent) => {
+        const cachedResponse = await caches.match(fetchEvent.request);
+        if (cachedResponse) {
+            Debug.debounceLog('[Service Worker] From cache: ' + fetchEvent.request.url);
+            return cachedResponse;
+        } else {
+            const responseFromFetch = await fetch(fetchEvent.request);
+            const cache = await caches.open(swConstants.CACHE_NAME);
+            if (!fetchEvent.request.url.includes('@')) {
+                // don't cache in development mode (the @ is used for the vite
+                // server files)
+                Debug.log('[Service Worker] Caching resource: ' + fetchEvent.request.url);
+                // this needs to be async, otherwise the response will be stalled
+                cache.put(fetchEvent.request, responseFromFetch.clone()).then();
+            }
+            return responseFromFetch;
+        }
+    };
+
+    fetchEvent.respondWith(responsePromise(fetchEvent),
+    );
+};
+
+
+self.addEventListener('activate', activateEventHandler);
+
+self.addEventListener('install', installEventHandler);
+
+self.addEventListener('fetch', fetchEventHandler);
+
+self.addEventListener('message', (event) => {
+    console.log(event);
+});
+
